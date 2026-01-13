62e7184f1424dcb492dc2a0bf1799705a0d24b0a
Valen-H <alternativexxxy@gmail.com>
2026-01-13 22:48:46 +0200

    QoL Update

--- README.md
+++ README.md
@@ -6,13 +6,15 @@ A tweak mod for Amazing Cultivation Simulator.
 
 - [x] Tweaks some grindy/slow aspects of the game like the Mental State (Mind), Practice Skill Exp Rate & Lower Attainment, Extreme Volatility of everything during GC breakthroughs, Feng Shui requirements (Blessed etc).
 - [x] Changes the cost of Skill Learning (Easy).
-- [x] Increases Base Move Speed of Disciples (+1).
-- [x] Makes slow events faster (City mainly).
+- [x] Increases Base Move Speed of Disciples (+1), and their skill acquisition rate (~x2).
+- [x] Makes slow events faster (City & Adventure mainly).
 - [x] Qi Gathering stabilization completes in 1 day instead of 5 days (optional: make sure you edit your other mods to display it correctly).
 - [x] Makes Law Compatibility Matches more realistic (Easy?).
 - [x] Pet Changing Delay is 1 day instead of 3 days.
 - [x] More viable Tribulation (Easy).
 - [x] More Manual Pavillion Storage Capacity.
+- [x] Faster Enlightened Animal Thinking & Increased Thoughts Cap (Easy?).
+- [x] No % Qi Consumption during Adventures.
 - [ ] Other Upcoming Easy-Mode tweaks to normalize the game and remove it's exponential boredom (& determinism).
 - [ ] Better Mortal Disciple Stats.
 - [ ] Training Facilities(?)
--- Tweaks.cs
+++ Tweaks.cs
@@ -1,9 +1,10 @@
-﻿using HarmonyLib;
-
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
+using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
-
+using HarmonyLib;
+using Newtonsoft.Json;
 using XiaWorld;
 
 namespace Acs_Tweaks {
@@ -102,7 +103,7 @@ namespace Acs_Tweaks {
 			
 			AccessTools.StaticFieldRefAccess<float>(typeof(LsStoneData), "_NeedT")		= 600f;
 			
-			KLog.Dbg("ACS Tweaks Loaded.");
+			KLog.Info("ACS Tweaks Loaded.");
 		} //OnInit
 		
 		[HarmonyPatch]
@@ -123,17 +124,23 @@ namespace Acs_Tweaks {
 				else
 					attainment	= UnityEngine.Mathf.Min(__instance.GetYinDaoHang(false), __instance.GetYangDaoHang(false));
 				
-				__result		= dif * (1f + attainment * (attainment / 2f)) * (1f + GameEventMgr.Instance.GetCurDriver().TreeExpAddtion) * (1f - CangJingGeMgr.Instance.GetCangJingGeLearnBuff());
+				__result		= dif * (3f + attainment * (attainment / 1.85f)) * (1f + GameEventMgr.Instance.GetCurDriver().TreeExpAddtion) * (1f - CangJingGeMgr.Instance.GetCangJingGeLearnBuff());
 				
 				return false;
 			} //GetTreeExpNeed
+			[HarmonyPostfix]
+			[HarmonyPatch(typeof(EsotericaData), nameof(EsotericaData.GetRealDifficulty))]
+			[MethodImpl(MethodImplOptions.NoInlining)]
+			static void EsotericaData_GetRealDifficulty(ref int __result) {
+				__result	= UnityEngine.Mathf.CeilToInt(__result / 1.4f);
+			} //EsotericaData_GetRealDifficulty
 			
 			[HarmonyPostfix]
 			[HarmonyPatch(typeof(Npc), nameof(Npc.GetSpeed))]
 			[MethodImpl(MethodImplOptions.NoInlining)]
-			static void GetSpeed(ref float __result) {
+			static void Npc_GetSpeed(ref float __result) {
 				__result++;
-			} //GetSpeed
+			} //Npc_GetSpeed
 			
 			[HarmonyPostfix]
 			[HarmonyPatch(typeof(OutspreadMgr), nameof(OutspreadMgr.Policy_Interval_Scale), MethodType.Getter)]
@@ -145,7 +152,7 @@ namespace Acs_Tweaks {
 			[HarmonyPrefix]
 			[HarmonyPatch(typeof(EffectMapData.MapEffectData), nameof(EffectMapData.MapEffectData.GetValue))]
 			[MethodImpl(MethodImplOptions.NoInlining)]
-			static bool GetValue(EffectMapData.MapEffectData __instance, ref float __result, int tkey, float bvalue = 0f, bool checkroom = true, bool checktime = false) {
+			static bool MapEffectData_GetValue(EffectMapData.MapEffectData __instance, ref float __result, int tkey, float bvalue = 0f, bool checkroom = true, bool checktime = false) {
 				if (bvalue == 0f)
 					bvalue				= __instance.basevalue;
 				
@@ -168,7 +175,7 @@ namespace Acs_Tweaks {
 				
 				__result				= (bvalue * (1f + __instance.addp) + __instance.addv) * UnityEngine.Mathf.Lerp(0f, 1f, (World.Instance.TolSecond - __instance.creattime) / 600f);
 				return false;
-			} //GetValue
+			} //MapEffectData_GetValue
 			
 			[HarmonyPrefix]
 			[HarmonyPatch(typeof(NpcPractice), nameof(NpcPractice.GetFiveBaseEfficiency), new Type[] { typeof(GongDef), typeof(Npc), typeof(g_emElementKind) })]
@@ -267,13 +274,6 @@ namespace Acs_Tweaks {
 			} //GetPenaltyFix2Cloud
 			
 			[HarmonyPostfix]
-			[HarmonyPatch(typeof(EsotericaData), nameof(EsotericaData.GetRealDifficulty))]
-			[MethodImpl(MethodImplOptions.NoInlining)]
-			static void GetRealDifficulty(ref int __result) {
-				__result	= UnityEngine.Mathf.RoundToInt(__result / 1.4f);
-			} //GetRealDifficulty
-			
-			[HarmonyPostfix]
 			[HarmonyPatch(typeof(CangJingGeMgr), MethodType.Constructor)]
 			[MethodImpl(MethodImplOptions.NoInlining)]
 			static void CangJingGeMgr_Ctor(object __instance) {
@@ -282,8 +282,285 @@ namespace Acs_Tweaks {
 				field?.SetValue(__instance, 200);
 			} //CangJingGeMgr_Ctor
 			
+			[HarmonyPostfix]
+			[HarmonyPatch(typeof(HumanoidEvolutionMgr), "GetThinkInterval")]
+			[MethodImpl(MethodImplOptions.NoInlining)]
+			static void GetThinkInterval(ref float __result) {
+				__result	/= 2.5f;
+			} //GetThinkInterval
+			[HarmonyPrefix]
+			[HarmonyPatch(typeof(HumanoidEvolutionMgr), "_NpcAddThink", new Type[] { typeof(Npc), typeof(HumanoidEvolutionMgr.ThinkFrag) })]
+			[MethodImpl(MethodImplOptions.NoInlining)]
+			static void NpcAddThink(HumanoidEvolutionMgr __instance, Npc npc) {
+				__instance.RaceInfos.GetDef(npc.RaceDefName).MaxThink	= 999;
+			} //NpcAddThink
+			[HarmonyPostfix]
+			[HarmonyPatch(typeof(HERaceInfoDef), MethodType.Constructor)]
+			[MethodImpl(MethodImplOptions.NoInlining)]
+			static void HERaceInfoDef(object __instance) {
+				var	field	= __instance.GetType().GetField("MaxThink", BindingFlags.Public | BindingFlags.Instance);
+				
+				field?.SetValue(__instance, 999);
+			} //HERaceInfoDef
+			
+			[HarmonyPostfix]
+			[HarmonyPatch(typeof(GongStageNeck), MethodType.Constructor)]
+			[MethodImpl(MethodImplOptions.NoInlining)]
+			static void GongStageNeck_Ctor(object __instance) {
+				var	field	= __instance.GetType().GetField("ExploreValue", BindingFlags.Public | BindingFlags.Instance);
+				
+				field?.SetValue(__instance, (float)field?.GetValue(__instance) / 3f);
+			} //GongStageNeck_Ctor
+			
+			[HarmonyPrefix]
+			[HarmonyPatch(typeof(NpcSkillData), nameof(NpcSkillData.AddSkillExp))]
+			[MethodImpl(MethodImplOptions.NoInlining)]
+			static void AddSkillExp(ref float v) {
+				v	+= 40;
+				v	*= 2.4f;
+				v	+= 200;
+			} //AddSkillExp
+			
+			[HarmonyPostfix]
+			[HarmonyPatch(typeof(PlacesMgr), nameof(PlacesMgr.GetCost))]
+			[MethodImpl(MethodImplOptions.NoInlining)]
+			static void PlacesMgr_GetCost(ref float __result) {
+				__result	/= 2f;
+			} //PlacesMgr_GetCost
+			
+			[HarmonyTranspiler]
+			[HarmonyPatch(typeof(JobLeave2Explore), "CheckWaitDie")]
+			[MethodImpl(MethodImplOptions.NoInlining)]
+			static IEnumerable<CodeInstruction> CheckWaitDie(IEnumerable<CodeInstruction> instructions) {
+				var	codes		= new List<CodeInstruction>(instructions);
+				var	gong		= AccessTools.PropertyGetter(typeof(Npc), nameof(Npc.GongKind));
+				var	ling		= AccessTools.PropertyGetter(typeof(Thing), nameof(Thing.MaxLing));
+				var	rand		= AccessTools.Method(typeof(World), nameof(World.RandomRange), new Type[] { typeof(float), typeof(float), typeof(GMathUtl.RandomType) });
+				var	add			= AccessTools.Method(typeof(Thing), nameof(Thing.AddLing));
+				
+				for (int i = 0; i < codes.Count; i++) {
+					if (i < codes.Count - 16 &&
+						codes[i].Calls(gong) &&
+						(codes[i + 2].opcode == OpCodes.Beq || codes[i + 2].opcode == OpCodes.Beq_S) &&
+						codes[i + 7].Calls(ling) &&
+						codes[i + 9].LoadsConstant(.1f) &&
+						codes[i + 10].LoadsConstant(.3f) &&
+						codes[i + 12].Calls(rand) &&
+						codes[i + 15].Calls(add)
+					) {
+						codes[i + 9].operand	= .001f;
+						codes[i + 10].operand	= .008f;
+						
+						KLog.Dbg("JobLeave2Explore.CheckWaitDie Patched.");
+						
+						break;
+					}
+				}
+				
+				return codes;
+			} //CheckWaitDie
+			
+			[HarmonyTranspiler]
+			[HarmonyPatch(typeof(ToilBrokenNeck), "OnEnterToil")]
+			[MethodImpl(MethodImplOptions.NoInlining)]
+			static IEnumerable<CodeInstruction> ToilBrokenNeck_OnEnterToil(IEnumerable<CodeInstruction> instructions) {
+				var	codes		= new List<CodeInstruction>(instructions);
+				var	thunder		= AccessTools.Field(typeof(GongStageNeck), nameof(GongStageNeck.ThunderValue));
+				
+				for (int i = 0; i < codes.Count; i++) {
+					if (i < codes.Count - 8 &&
+						codes[i].LoadsField(thunder) &&
+						codes[i + 7].LoadsConstant(520000f)) {
+						codes[i + 7].operand	= 400000f;
+						
+						KLog.Dbg("ToilBrokenNeck.OnEnterToil Patched.");
+						
+						break;
+					}
+				}
+				
+				return codes;
+			} //ToilBrokenNeck_OnEnterToil
+			
 		} //Nerf
+		
+		[HarmonyPatch]
+		static class Fixes {
+			
+			[HarmonyPrefix]
+			[HarmonyPatch(typeof(JobMgr), "GetJobClass")]
+			[MethodImpl(MethodImplOptions.NoInlining)]
+			static bool GetJobClass(ref JobBase __result, Dictionary<string, List<JobBase>> ___m_mapJobCache, string name) {
+				if (___m_mapJobCache.TryGetValue(name, out List<JobBase> list) && list.Count > 0) {
+					JobBase	jobBase	= list[list.Count - 1];
+					
+					list.RemoveAt(list.Count - 1);
+					
+					__result	= jobBase;
+					
+					return false;
+				}
+				
+				JobDef	jobDef		= AccessTools.StaticFieldRefAccess<Dictionary<string, JobDef>>(typeof(JobMgr), "JobDefs")[name];
+				Type	type		= null;
+				string	typeName	= "XiaWorld." + jobDef.DriverClass;
+				
+				foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies()) {
+					type	= assembly.GetType(typeName, false, true);
+					
+					if (type != null)
+						break;
+				}
+				
+				if (type == null)
+					throw new TypeLoadException("Could not load Job type:\t" + typeName);
+				
+				__result	= (JobBase)Activator.CreateInstance(type);
+				
+				return false;
+			} //GetJobClass
+			
+			[HarmonyPostfix]
+			[HarmonyPatch(typeof(Npc), "InitDiscipleBehaviour")]
+			[MethodImpl(MethodImplOptions.NoInlining)]
+			static void InitDiscipleBehaviour(Npc __instance) {
+				__instance.JobEngine.AddBehaviour(new BehaviourACSTrain(g_emBehaviourKind.Magic, g_emBehaviourWorkKind.None, 3));
+			} //InitDiscipleBehaviour
+			
+		} //Fixes
 		
 	} //Acs_Tweaks
 	
 } //Acs_Tweaks
+
+namespace XiaWorld {
+	
+	[JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+	public class JobACSTrain : JobBase {
+		
+		protected override void OnInit(params object[] objs) {
+			KLog.Dbg("JobACSTrain.OnInit Called.");
+		} //OnInit
+		protected override void OnEnterJob(KStateQUnit unit) {
+			KLog.Dbg("JobACSTrain.OnEnterJob Called.");
+		} //OnEnterJob
+		protected override void OnLeaveJob(KStateQUnit unit) {
+			CMD?.FinishCommand(false, false, false);
+			Worker.JumpOutFromBuilding(false);
+			KLog.Dbg("JobACSTrain.OnLeaveJob Called.");
+		} //OnLeaveJob
+		
+		protected override List<ToilBase> GetToilList() {
+			BuildingThing	buildingThing	= ThingMgr.Instance.FindThingByID((int)CMD.WorkParam) as BuildingThing;
+			List<ToilBase>	list			= new List<ToilBase>();
+			int				num				= buildingThing.CheckWorkSpace(true);
+			
+			list.Add(new ToilLockWorkSpace(buildingThing, num, false));
+			list.Add(ToilGoto.GotoThing(buildingThing, g_emPathEndMode.Touch, num, false, null));
+			list.Add(new ToilJump2Building(buildingThing, num, false));
+			list.Add(new ToilACSTrain());
+			list.Add(new ToilJump2Building(null, 0, true));
+			
+			return list;
+		} //GetToilList
+		protected override void OnToilFinish(ToilBase toil, g_emJobToilState state) {
+			KLog.Dbg("JobACSTrain.OnToilFinish Called.");
+		} //OnToilFinish
+		
+		protected override bool OnCheckFaildCondition() {
+			return true;
+		} //OnCheckFaildCondition
+		
+	} //JobACSTrain
+	
+	public class BehaviourACSTrain : BehaviourBase {
+		
+		public BehaviourACSTrain(g_emBehaviourKind kind, g_emBehaviourWorkKind workkind, int MPriority) : base(kind, workkind, MPriority) { } //BehaviourACSTrain
+		
+		public override JobBase Check(Npc npc, int seachr = 10000, bool tryfind = false) {
+			if (npc.HealthState != g_emNpcHealthState.Normal)
+				return null;
+			
+			Command	command	= CommandMgr.Instance.FindCommandAtThing("ACSTrain", npc, npc);
+			
+			if (command != null)
+				return JobMgr.Instance.CreateJob("JobACSTrain", command, new object[0]);
+			
+			return null;
+		} //Check
+		
+	} //BehaviourACSTrain
+	
+	[JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+	public class CommandACSTrain : Command {
+		
+		protected override void OnInit(params object[] objs) {
+			base.OnInit(objs);
+			KLog.Dbg("CommandACSTrain.OnInit Called.");
+		} //OnInit
+		public override void OnAfterLoad() {
+			KLog.Dbg("CommandACSTrain.OnAfterLoad Called.");
+		} //OnAfterLoad
+		protected override void OnFinished(bool did) {
+			KLog.Dbg("CommandACSTrain.OnFinished Called.");
+		} //OnFinished
+		
+		protected override bool CheckRemoveAble() {
+			if (!(OwnerThing is Npc npc) || !npc.IsValid || !npc.IsDisciple)
+				return true;
+			
+			return false;
+		} //CheckRemoveAble
+		protected override bool CouldBeFind(Npc npc) {
+			Npc	npc2	= OwnerThing as Npc;
+			
+			if (!npc2.IsCorpse && !npc2.IsDeath && CouldBeFind(npc)) {
+				BuildingThing	buildingThing	= npc.map.Things.FindBuilding(npc, 9999, "SkillPractice", 0, true, false, 0, 9999, null, null, true);
+				
+				if (buildingThing != null && npc.map.CheckPath(npc, buildingThing.Key, g_emPathEndMode.Touch, false, true, true) > 0) {
+					WorkParam = buildingThing.ID;
+					
+					return true;
+				}
+			}
+			
+			return false;
+		} //CouldBeFind
+		
+	} //CommandACSTrain
+	
+	[JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+	public class ToilACSTrain : ToilBase {
+		
+		public override string	ToilAnimation {
+			get		=> "shuhua1";
+		} //ToilAnimation
+		
+		[JsonProperty]
+		protected float			costtime;
+		
+		public override string GetDesc()	=> TFMgr.Get("Train Skills"); //GetDesc
+		public override string GetToilKey()	=> "acstrain"; //GetToilKey
+		
+		protected override void OnNpcStoryTrigger() {
+			KLog.Dbg("ToilACSTrain.OnNpcStoryTrigger Called.");
+		} //OnNpcStoryTrigger
+		
+		protected override void OnEnterToil(KStateQUnit unit) {
+			KLog.Dbg("ToilACSTrain.OnEnterToil Called.");
+			
+			costtime	= 50;
+		} //OnEnterToil
+		protected override void OnStepToil(float dt, KStateQUnit unit) {
+			SetProgress(unit.keeptime / costtime);
+			
+			if (unit.keeptime >= costtime) {
+				m_bDid			= true;
+				unit.IsFinished	= true;
+			}
+		} //OnStepToil
+		protected override void OnLeaveToil(KStateQUnit unit) { } //OnLeaveToil
+		
+	} //ToilACSTrain
+	
+} //XiaWorld
